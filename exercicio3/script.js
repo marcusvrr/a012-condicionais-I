57a66091fb9dc7090767fd548a95c1c13366b528
marcusvrr <marcusvrr2@gmail.com>
2022-08-03 23:27:25 -0300

    exercicio3 concluido

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,13 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade") //= adicione o prompt aqui
+
+if (nacionalidade.toLowerCase() === "brasileiro"){
+    console.log('Você é brasileiro')
+ }else if (nacionalidade.toLowerCase() === "argentino") {
+    console.log('Você é argentino')
+}else if (nacionalidade.toLowerCase() === "uruguaio") {
+    console.log('Você é uruguaio')
+}else if (nacionalidade.toLowerCase() === "chicleno") {
+    console.log('Você é chicleno')
+}else if (nacionalidade.toLowerCase() === "colombiano") {
+    console.log('Você é colombiano')
+}else {console.log('Nacionalidade não encontrada!') }
