5760ac40538282968cdb874d36f2069e7b0d5ae6
marcusvrr <marcusvrr2@gmail.com>
2022-08-04 21:18:46 -0300

    exercicio pós- feedback

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,13 @@
 let nacionalidade = prompt("Escreva aqui sua nacionalidade") //= adicione o prompt aqui
 
-if (nacionalidade.toLowerCase() === "brasileiro"){
-    console.log('Você é brasileiro')
- }else if (nacionalidade.toLowerCase() === "argentino") {
-    console.log('Você é argentino')
-}else if (nacionalidade.toLowerCase() === "uruguaio") {
-    console.log('Você é uruguaio')
-}else if (nacionalidade.toLowerCase() === "chicleno") {
-    console.log('Você é chicleno')
-}else if (nacionalidade.toLowerCase() === "colombiano") {
-    console.log('Você é colombiano')
+if (nacionalidade.toLowerCase() === "brasileiro"||nacionalidade.toLowerCase() === "brasileira"){
+    console.log('Você é brasileiro(a)')
+ }else if (nacionalidade.toLowerCase() === "argentino"||nacionalidade.toLowerCase() === "argentina") {
+    console.log('Você é argentino(a)')
+}else if (nacionalidade.toLowerCase() === "uruguaio"||nacionalidade.toLowerCase() === "uruguaia") {
+    console.log('Você é uruguaio(a)')
+}else if (nacionalidade.toLowerCase() === "chileno"||nacionalidade.toLowerCase() === "chilena") {
+    console.log('Você é chicleno(a)')
+}else if (nacionalidade.toLowerCase() === "colombiano"||nacionalidade.toLowerCase() === "colombiana") {
+    console.log('Você é colombiano(a)')
 }else {console.log('Nacionalidade não encontrada!') }
